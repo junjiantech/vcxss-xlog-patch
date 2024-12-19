88caffc0713798491d44a0d2dfbea8ca7aa8e95b
junjian <cnrivkaer@outlook.com>
2024-12-19 14:29:00 +0800

    feat: 调整 mars-xlog 相关代码

--- app/src/main/java/com/vcxss/xlog/patch/sample/MainActivity.kt
+++ app/src/main/java/com/vcxss/xlog/patch/sample/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.facebook.soloader.SoLoader
 import com.tencent.mars.xlog.Xlog
-import com.tencent.mars.xlog.Xlog.XLogConfig
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
@@ -19,37 +18,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // 初始化
         SoLoader.init(applicationContext, true)
-        // 加载动态库
-        SoLoader.loadLibrary("c++_shared")
-        SoLoader.loadLibrary("marsxlog")
-
-        val logConfig = XLogConfig()
-        logConfig.logdir = File(cacheDir, "log").absolutePath
-        logConfig.cachedir = File(cacheDir, "log_cache").absolutePath
-        logConfig.nameprefix = "Xlog"
-        logConfig.compresslevel = 6
-        logConfig.compressmode = Xlog.ZLIB_MODE
-        logConfig.level = Xlog.LEVEL_INFO
-        logConfig.cachedays = 7
-        logConfig.mode = Xlog.AppednerModeAsync
+        // 安全加载仓库
+        Xlog.safeModelLoadLibrary()
         // 创建Xlog实例
         val xlog = Xlog()
         // 开启console
         xlog.setConsoleLogOpen(0, true)
         // 设置单文件大小
-        xlog.setMaxFileSize(0, 10 * 1024 * 1024)
+        xlog.setMaxFileSize(0, 5 * 1024 * 1024)
         // 设置最长有效天数
         xlog.setMaxAliveTime(0, 7 * 24 * 60 * 60)
-        // 开启xlogApper
-        xlog.openAppender(logConfig)
-        val filename = "vlog"
+        // 日志Appender配置
+        val logConfig = Xlog.XLogConfig()
+        logConfig.logdir = File(cacheDir, "logs").absolutePath
+        logConfig.cachedir = File(cacheDir, "log_cache").absolutePath
+        logConfig.nameprefix = "Xlog"
+        logConfig.compresslevel = 6
+        logConfig.compressmode = Xlog.ZLIB_MODE
+        logConfig.level = Xlog.LEVEL_INFO
+        logConfig.cachedays = 1
+        logConfig.mode = Xlog.AppednerModeAsync
+        // 开启日志追加
+        xlog.appenderOpen(logConfig)
         val pid = Process.myPid()
         val threadId = Thread.currentThread().id
         val mainTid = Looper.getMainLooper().thread.id
         Thread {
             while (true) {
-                xlog.logI(0, "Vlog", filename, "", 0, pid, threadId, mainTid, "IIIIIIIII")
-                Thread.sleep(500)
+                try {
+                    xlog.logI("Vlog", pid, threadId, mainTid, "sys time: ${System.currentTimeMillis()}")
+                    var i = 1/ 0
+                }catch (ex: Exception){
+                    xlog.logE("Vlog", pid, threadId, mainTid, ex.message + "\n" + ex.stackTraceToString())
+                }
+                Thread.sleep(100)
             }
         }.start()
 
--- build_xlog.bat
+++ build_xlog.bat
@@ -1,9 +1,16 @@
-
+@echo off
+$env:NDK_ROOT="D:\environment\Android\ndk\20.0.5594570"
 @REM clone mars
 git clone https://github.com/Tencent/mars.git
+@REM 应用变更
+cd mars
+git apply ../0001-local-change-xlog.patch
 @REM 进入xlog编译目录
-CD mars/mars
+CD mars
 @REM 编译xlog二进制文件
 python build_android.py "--target libzstd_static marsxlog" "armeabi-v7a" "arm64-v8a"
 @REM 移动libs二进制文件
 cp -r libraries/mars_android_sdk/libs/* ../../libs-xlog/src/main/jniLibs/
+@REM 删除仓库
+cd ../../
+Remove-Item ".\mars" -Recurse
